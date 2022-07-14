aa49e56c7bd642647474caf933582e15d3dc981d
dmitry.sokolov <dmitry.sokolov@jetrockets.ru>
2022-07-14 14:09:22 +0300

    Refactored linter configs

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import React from 'react';
 import Button from 'components/Button/Button';
 import Logo from 'assets/images/esbuild-logo.png';
 
-
-import "./style.css"
+import './style.css';
 
 const App = () => {
   const handleClick = () => {
     throw new Error();
   };
+
+  const tes = { a: 1 };
 
   return (
     <>
